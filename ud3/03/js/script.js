fa44572bdf6a2b149df2c38c4a737cfa7dedadbb
Alvaro <alvaro.allper.1@educa.jcyl.es>
2025-06-05 20:20:41 +0200

    Actividad 3 Tema 3 realizada

--- ud3/03/js/script.js
+++ ud3/03/js/script.js
@@ -1,64 +1,20 @@
-/*Definición de las imágenes en un vector.*/ 
-const imagenes = [
-    'https://picsum.photos/200?random=1',
-    'https://picsum.photos/200?random=2',
-    'https://picsum.photos/200?random=3',
-    'https://picsum.photos/200?random=4',
-    'https://picsum.photos/200?random=5',
-    'https://picsum.photos/200?random=6',
-    'https://picsum.photos/200?random=7',
-    'https://picsum.photos/200?random=8'
-];
+const contenedor = document.getElementById('tablero');
+contenedor.innerHTML = '<div><img src="images/B.jpg" onclick="cambiarImagen(this)"/>'
++'<img src="images/B.jpg" onclick="cambiarImagen(this)"/>'
++'<img src="images/B.jpg" onclick="cambiarImagen(this)"/></div>'
++'<div><img src="images/B.jpg" onclick="cambiarImagen(this)"/>'
++'<img src="images/B.jpg" onclick="cambiarImagen(this)"/>'
++'<img src="images/B.jpg" onclick="cambiarImagen(this)"/></div>';
 
-
-function crearTablero(){
-    const tablero = document.getElementById("tablero");
-
-    for (let i = 0; i < 2; i++){
-        const fila = document.createElement("div");
-        fila.classList.add("fila");
-
-        for (let j = 0; j < 3; j++){
-            const columna = document.createElement("div");
-            columna.classList.add("casilla");
-        
-    
-
-    const textHover = document.createElement("p");
-
-    textHover.textContent = "Pulsa para mostrar imagen";
-    textHover.style.display = "none";
-    columna.appendChild(textHover);
-
-    columna.addEventListener("mouseover", function() {
-        if(!columna.querySelector("img")){
-            textoHover.style.display= "block";
-        }
-    });
-
-    columna.addEventListener("mouseout", function(){
-        textHover.style.display = "none";
-    });
-
-            columna.addEventListener("click", function(){
-                if(!columna.addEventListener("img")){
-                    const img = document.createElement("img");
-                    const numero = Math.floor(Math.random() * imagenes.length);
-                    img.src = `https://picsum.photos/200?random=${numero}`;
-                    img.classList.add("imagen");
-                    columna.appendChild(img);
-
-                    textHover.style.display = "none";
-                } else{
-                    const numero = Math.floor(Math.random() * imagenes.length);
-                    columna.querySelector("img").src = `https://picsum.photos/200?random=${numero}`;
-                }
-            });
-
-            fila.appendChild(columna);
-        }
-        tablero.appendChild(fila);
-        }
-    }
-
-window.onload = crearTablero;
+function cambiarImagen(imagen) {
+    const imagenes = [ 'https://picsum.photos/200?random=1', 
+        'https://picsum.photos/200?random=2', 
+        'https://picsum.photos/200?random=3', 
+        'https://picsum.photos/200?random=4', 
+        'https://picsum.photos/200?random=5', 
+        'https://picsum.photos/200?random=6', 
+        'https://picsum.photos/200?random=7', 
+        'https://picsum.photos/200?random=8' 
+    ];
+    imagen.src = imagenes[Math.floor(Math.random() * imagenes.length)];
+}
